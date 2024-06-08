5173fabe9f2146c453f2bef1c8f69f5534c21575
ElnurBX <tu8dhdtwy@code.edu.az>
2024-06-08 21:13:11 +0400

    dfadfasdf

--- backend/models/Order.model.js
+++ backend/models/Order.model.js
@@ -3,7 +3,6 @@ const mongoose = require("mongoose");
 const Order = mongoose.model("Order", new mongoose.Schema({
   title: {
     type: String,
-    required: true // Bu alanın gerekli olduğundan emin olun
   },
 }));
 
